0d905a6d3d346c0ffd0fa89c538eda6375752f55
Sayantan Ghosh <sayantan.ghosh03@gmail.com>
2025-06-29 18:22:46 +0530

    Added CORS config in /expenses-category/all API

--- app/api/expenses-category/all/route.ts
+++ app/api/expenses-category/all/route.ts
@@ -1,12 +1,28 @@
 import { NextResponse } from "next/server";
 import { supabase } from "@/lib/supabase";
+import { getCorsHeaders } from "@/lib/cors";
+
+// Preflight handler
+export async function OPTIONS(req: NextRequest) {
+  const origin = req.headers.get("origin") || "";
+  const headers = getCorsHeaders(origin);
+
+  return new NextResponse(null, {
+    status: 204,
+    headers,
+  });
+}
 
 export async function GET() {
+  const headers = getCorsHeaders(origin);
   const { data, error } = await supabase.from("expenses_category").select("*");
 
   if (error) {
     console.error("RPC error:", error.message);
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return new NextResponse(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers,
+    });
   }
 
   return NextResponse.json({ data }, { status: 200 });
