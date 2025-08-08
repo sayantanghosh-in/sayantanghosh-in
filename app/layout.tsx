2e2b9e03dff8d990b2b44cb19afc088c2e00b365
Sayantan Ghosh <sayantan.ghosh03@gmail.com>
2025-08-08 17:46:42 +0530

    Lots of very new things

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
