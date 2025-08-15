2328afc6f7f6c42e0a97ff4652327347788f80c6
Sayantan Ghosh <sayantan.ghosh03@gmail.com>
2025-08-15 13:58:21 +0530

    Moved on to pnpm. Updated some metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export const metadata: Metadata = {
     siteName: "Sayantan Ghosh",
     images: [
       {
-        url: "https://sayantanghosh.in/sayantan.png",
-        width: 320,
-        height: 320,
+        url: "https://sayantanghosh.in/banner.png",
+        width: 943,
+        height: 221,
         alt: "Sayantan Ghosh Website Open Graph Image",
       },
     ],
@@ -34,6 +34,9 @@ export const metadata: Metadata = {
     "Sayantan Ghosh India",
     "Sayantan Ghosh Bengaluru",
     "Sayantan Ghosh Bangalore",
+    "Sayantan Ghosh Karnataka",
+    "Sayantan Ghosh Bengaluru Karnataka",
+    "Sayantan Ghosh Bangalore Karnataka",
     "Front end",
     "Developer",
     "Engineer",
@@ -54,7 +57,7 @@ export const metadata: Metadata = {
       "My name is Sayantan Ghosh. I am a front end engineer specializing in React.JS, Typescript, Next.JS and various other technologies like Express.JS, Node.JS, PostgreSQL, etc.",
     site: "@sayantan__ghosh",
     creator: "@sayantan__ghosh",
-    images: ["https://sayantanghosh.in/sayantan.png"],
+    images: ["https://sayantanghosh.in/banner.png"],
   },
 };
 
