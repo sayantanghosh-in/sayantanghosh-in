33fe93a5d80a2127d0bc664c2868cda13b79d710
Sayantan Ghosh <sayantan.ghosh03@gmail.com>
2025-08-24 11:56:47 +0530

    revert: project gif beside title

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,51 +1,50 @@
-import { IconBrandGithub, IconPoint, IconWriting } from "@tabler/icons-react";
+import {
+  IconExternalLink,
+  IconBrandGithub,
+  IconPoint,
+  IconWriting,
+} from "@tabler/icons-react";
 import Link from "next/link";
 import { projects } from "@/lib/constants";
-import Image from "next/image";
 
 export const Projects = () => {
   return projects?.map((project) => {
     return (
       <section key={project?.id} className="border-t-1 px-2 md:px-32 lg:px-96">
         <div className="border-x-1 p-2">
-          <div className="flex items-start gap-2">
-            <div className="flex flex-col">
-              <h3 className="text-lg font-semibold mb-0.5 underline max-w-[150px] text-wrap break-all">
-                <Link href={project?.urls?.landingPage}>{project?.title}</Link>
-              </h3>
-              <p className="text-xs text-muted-foreground mb-1">
-                {project?.timeline}
-              </p>
-              <div className="flex gap-2 items-center mb-2">
-                <Link
-                  aria-label="GotoDash github"
-                  target="_blank"
-                  href={project?.urls?.github}
-                >
-                  <IconBrandGithub
-                    size={16}
-                    stroke={1.5}
-                    color="var(--accent-foreground)"
-                  />
-                </Link>
-                <Link
-                  aria-label="GotoDash blog post"
-                  href={project?.urls?.blogPost}
-                >
-                  <IconWriting
-                    size={16}
-                    stroke={1.5}
-                    color="var(--accent-foreground)"
-                  />
-                </Link>
-              </div>
-            </div>
-            <Image
-              alt="project gif"
-              src={project?.urls?.thumbnail}
-              width={150}
-              height={50}
+          <h3 className="text-lg font-semibold mb-0.5 underline flex items-center gap-1 max-w-[150px] text-wrap break-all">
+            <Link href={project?.urls?.landingPage}>{project?.title}</Link>
+            <IconExternalLink
+              size={14}
+              stroke={2}
+              color="var(--accent-foreground)"
             />
+          </h3>
+          <p className="text-xs text-muted-foreground mb-1">
+            {project?.timeline}
+          </p>
+          <div className="flex gap-2 items-center mb-2">
+            <Link
+              aria-label="GotoDash github"
+              target="_blank"
+              href={project?.urls?.github}
+            >
+              <IconBrandGithub
+                size={16}
+                stroke={1.5}
+                color="var(--accent-foreground)"
+              />
+            </Link>
+            <Link
+              aria-label="GotoDash blog post"
+              href={project?.urls?.blogPost}
+            >
+              <IconWriting
+                size={16}
+                stroke={1.5}
+                color="var(--accent-foreground)"
+              />
+            </Link>
           </div>
           <p className="text-md mb-2">{project?.description}</p>
           <div className="flex flex-col gap-1">
