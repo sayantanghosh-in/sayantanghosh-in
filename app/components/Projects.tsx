b80c2e7dfea768377101dd072e05735fd7fc56e7
Sayantan Ghosh <sayantan.ghosh03@gmail.com>
2025-08-24 11:47:35 +0530

    fix + ux enhancement
    fix: project bullet point style
    enhancement: added project gif beside name

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,62 +1,63 @@
-import {
-  IconBrandGithub,
-  IconExternalLink,
-  IconPoint,
-  IconWriting,
-} from "@tabler/icons-react";
+import { IconBrandGithub, IconPoint, IconWriting } from "@tabler/icons-react";
 import Link from "next/link";
 import { projects } from "@/lib/constants";
+import Image from "next/image";
 
 export const Projects = () => {
   return projects?.map((project) => {
     return (
       <section key={project?.id} className="border-t-1 px-2 md:px-32 lg:px-96">
         <div className="border-x-1 p-2">
-          <h3 className="text-lg font-semibold mb-0.5 underline flex items-center gap-1">
-            <Link href={project?.urls?.landingPage}>{project?.title}</Link>
-            <IconExternalLink
-              size={14}
-              stroke={2}
-              color="var(--accent-foreground)"
+          <div className="flex items-start gap-2">
+            <div className="flex flex-col">
+              <h3 className="text-lg font-semibold mb-0.5 underline max-w-[150px] text-wrap break-all">
+                <Link href={project?.urls?.landingPage}>{project?.title}</Link>
+              </h3>
+              <p className="text-xs text-muted-foreground mb-1">
+                {project?.timeline}
+              </p>
+              <div className="flex gap-2 items-center mb-2">
+                <Link
+                  aria-label="GotoDash github"
+                  target="_blank"
+                  href={project?.urls?.github}
+                >
+                  <IconBrandGithub
+                    size={16}
+                    stroke={1.5}
+                    color="var(--accent-foreground)"
+                  />
+                </Link>
+                <Link
+                  aria-label="GotoDash blog post"
+                  href={project?.urls?.blogPost}
+                >
+                  <IconWriting
+                    size={16}
+                    stroke={1.5}
+                    color="var(--accent-foreground)"
+                  />
+                </Link>
+              </div>
+            </div>
+            <Image
+              alt="project gif"
+              src={project?.urls?.thumbnail}
+              width={150}
+              height={50}
             />
-          </h3>
-          <p className="text-xs text-muted-foreground mb-1">
-            {project?.timeline}
-          </p>
-          <div className="flex gap-2 items-center mb-2">
-            <Link
-              aria-label="GotoDash github"
-              target="_blank"
-              href={project?.urls?.github}
-            >
-              <IconBrandGithub
-                size={16}
-                stroke={1.5}
-                color="var(--accent-foreground)"
-              />
-            </Link>
-            <Link
-              aria-label="GotoDash blog post"
-              href={project?.urls?.blogPost}
-            >
-              <IconWriting
-                size={16}
-                stroke={1.5}
-                color="var(--accent-foreground)"
-              />
-            </Link>
           </div>
           <p className="text-md mb-2">{project?.description}</p>
           <div className="flex flex-col gap-1">
             {project?.highlights?.map((highlight, highlightIndex) => {
               return (
-                <p
+                <div
                   key={`project-${project?.id}-highlight-${highlightIndex}`}
-                  className="text-sm flex items-center gap-2"
+                  className="text-sm flex items-center gap-1"
                 >
-                  <IconPoint size={14} />
-                  {highlight}
-                </p>
+                  <IconPoint size={12} />
+                  <p className="w-[calc(100%-12px)]">{highlight}</p>
+                </div>
               );
             })}
           </div>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://cdn.loom.com/**")],
+  },
 };
 
 export default nextConfig;
