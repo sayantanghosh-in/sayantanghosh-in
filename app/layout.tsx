d7632a0622112d12b5fdb1dda49e6f6474f61749
Sayantan Ghosh <sayantan.ghosh03@gmail.com>
2025-08-26 17:58:27 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="favicon.svg" type="image/svg+xml" />
       </head>
       <body className={inter.className}>
         <div>
